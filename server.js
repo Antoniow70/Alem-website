943030e47987f48b5a1c5c92d65990f005300cba
Paulo Tivane <tivanepaulo2@gmail.com>
2026-06-20 08:28:11 +0200

    refactor: renomear frontend para client, backend para server, apagar server.js legado da raiz

--- server.js
+++
@@ -1,3 +0,0 @@
-// AVISO: Este arquivo é legado da estrutura antiga do projeto.
-// O servidor Express atual e ativo está localizado em backend/src/server.js.
-// Este arquivo pode ser excluído com segurança.
